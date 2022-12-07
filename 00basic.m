c331538417a20e64c89e3e139fe87e969111b703
Monmoy042 <monmoykms.16@gmail.com>
2022-12-07 21:10:49 +0600

    MATLAB basic and variable added

---
+++ 00basic.m
@@ -0,0 +1,12 @@
+a=100
+b=50
+a+b
+a-b
+a*b
+a/b
+
+##clc
+##clear a
+##clear b
+
+
---
+++ 01variable.m
@@ -0,0 +1,22 @@
+clc;
+clear all;
+close all;
+
+a = 50;
+b = 20;
+
+#addition = a+b;
+#subtruction = a-b;
+#multiplication = a*b;
+#division = a/b
+
+sqr = sqrt(16);
+piValue = pi;
+logValue = log(1);
+powerValue = power(5,2);
+remValue= rem(5,3);
+
+round(5.501)
+floor(5.301)
+ceil(5.101)
+
